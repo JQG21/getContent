30cc5dc8751fa617d9872f4626832265c155bb0a
succeessful <18212325202@163.com>
2020-09-27 19:03:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/getcontent/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.getcontent;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private ContentResolver resolver;
+    private String TAG = "MyContent";
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.get);
+
+        resolver = this.getContentResolver();
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+                while (cursor.moveToNext()){
+                    String msg;
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    msg = "id:" + id;
+
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    msg = msg + " name:" + name;
+
+                    Cursor phoneNumbers = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id, null, null);
+                    while (phoneNumbers.moveToNext()){
+                        String phoneNumber = phoneNumbers.getString(phoneNumbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        msg = msg + " phone:" + phoneNumber;
+                    }
+                    Log.v(TAG,msg);
+                }
+            }
+        });
+    }
+    }
